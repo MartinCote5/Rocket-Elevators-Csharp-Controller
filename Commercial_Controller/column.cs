e2d47726bb04458f09b8e4e86b8925480cf0152d
MartinCote5 <Martin.Cote2@hotmail.com>
2022-03-01 16:46:19 -0500

    find elevator 1 iteration done

--- Commercial_Controller/column.cs
+++ Commercial_Controller/column.cs
@@ -68,8 +68,44 @@ namespace Commercial_Controller
  
         public Elevator findElevator(int requestedFloor, string requestedDirection) {
 
+            Elevator bestElevator = null;
+            int bestScore = 6;
+            int referenceGap = 10000000;
+            Elevator bestElevatorInformations;
 
-            Console.WriteLine(requestedDirection);
+                if (requestedFloor == 1) {
+                    foreach (Elevator elevator in elevatorsList) {
+                        //The elevator is at the lobby and already has some requests. It is about to leave but has not yet departed
+                        if (1 == elevator.currentFloor && elevator.status == "stopped") {
+                            bestElevatorInformations = checkIfElevatorIsBetter(1, elevator, bestScore, referenceGap, bestElevator, requestedFloor);       
+                        }
+                    }
+                }
+           
+                
+                
+            //         SET bestElevatorInformations TO CALL THIS checkIfElevatorIsBetter WITH 1 AND elevator AND bestScore AND referenceGap AND bestElevator AND requestedFloor RETURNING bestElevatorInformations
+            //     '//The elevator is at the lobby and has no requests
+            //     ELSE IF 1 EQUALS elevator currentFloor AND elevator status EQUALS idle THEN
+            //         SET bestElevatorInformations TO CALL THIS checkIfElevatorIsBetter WITH 2 AND elevator AND bestScore AND referenceGap AND bestElevator AND requestedFloor RETURNING bestElevatorInformations
+            //     '//The elevator is lower than me and is coming up. It means that I'm requesting an elevator to go to a basement, and the elevator is on it's way to me.
+            //     ELSE IF 1 IS GREATER THAN elevator currentFloor AND elevator direction EQUALS up THEN
+            //         SET bestElevatorInformations TO CALL THIS checkIfElevatorIsBetter WITH 3 AND elevator AND bestScore AND referenceGap AND bestElevator AND requestedFloor RETURNING bestElevatorInformations
+            //     '//The elevator is above me and is coming down. It means that I'm requesting an elevator to go to a floor, and the elevator is on it's way to me
+            //     ELSE IF 1 IS LESS THAN elevator currentFloor AND elevator direction EQUALS down THEN
+            //         SET bestElevatorInformations TO CALL THIS checkIfElevatorIsBetter WITH 3 AND elevator AND bestScore AND referenceGap AND bestElevator AND requestedFloor RETURNING bestElevatorInformations
+            //     '//The elevator is not at the first floor, but doesn't have any request
+            //     ELSE IF elevator status EQUALS idle THEN
+            //         SET bestElevatorInformations TO CALL THIS checkIfElevatorIsBetter WITH 4 AND elevator AND bestScore AND referenceGap AND bestElevator AND requestedFloor RETURNING bestElevatorInformations
+            //     '//The elevator is not available, but still could take the call if nothing better is found
+            //     ELSE 
+            //         SET bestElevatorInformations TO CALL THIS checkIfElevatorIsBetter WITH 5 AND elevator AND bestScore AND referenceGap AND bestElevator AND requestedFloor RETURNING bestElevatorInformations
+            //     ENDIF
+            //     SET bestElevator TO bestElevatorInformations bestElevator
+            //     SET bestScore TO bestElevatorInformations bestScore
+            //     SET referenceGap TO bestElevatorInformations referenceGap
+            // ENDFOR
+            
 
 
             Elevator x = null;
@@ -79,6 +115,35 @@ namespace Commercial_Controller
         }
 
     
+        public  Elevator checkIfElevatorIsBetter(int scoreToCheck, Elevator newElevator, int bestScore, int referenceGap, Elevator bestElevator, int Floor ) {
+            
+            Elevator bestElevatorInformations = null;
+            return bestElevatorInformations;
+
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
