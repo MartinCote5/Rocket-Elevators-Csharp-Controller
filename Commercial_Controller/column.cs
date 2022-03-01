e19e2b1c6ad66262131207c145d3f9431ec7e6e1
MartinCote5 <Martin.Cote2@hotmail.com>
2022-03-01 18:55:34 -0500

    find elevator and check elevator done

--- Commercial_Controller/column.cs
+++ Commercial_Controller/column.cs
@@ -68,31 +68,31 @@ namespace Commercial_Controller
  
         public Elevator findElevator(int requestedFloor, string requestedDirection) {
 
-            Elevator bestElevator = elevatorsList[0];
+            Elevator bestElevator = elevatorsList[1];
             int bestScore = 6;
             int referenceGap = 10000000;
-            Elevator bestElevatorInformations;
+            // Elevator bestElevatorInformations;
 
                 if (requestedFloor == 1) {
                     foreach (Elevator elevator in elevatorsList) {
                         //The elevator is at the lobby and already has some requests. It is about to leave but has not yet departed
                         if (1 == elevator.currentFloor && elevator.status == "stopped") {
-                            bestElevatorInformations = checkIfElevatorIsBetter(1, elevator, bestScore, referenceGap, bestElevator, requestedFloor);    
+                            (bestElevator, bestScore, referenceGap) = checkIfElevatorIsBetter(1, elevator, bestScore, referenceGap, bestElevator, requestedFloor);    
                             //The elevator is at the lobby and has no requests   
                     }   else if (1 == elevator.currentFloor && elevator.status == "idle") {
-                            bestElevatorInformations = checkIfElevatorIsBetter(2, elevator, bestScore, referenceGap, bestElevator, requestedFloor);
+                            (bestElevator, bestScore, referenceGap) = checkIfElevatorIsBetter(2, elevator, bestScore, referenceGap, bestElevator, requestedFloor);
                             //The elevator is lower than me and is coming up. It means that I'm requesting an elevator to go to a basement, and the elevator is on it's way to me
                     }   else if (1 > elevator.currentFloor && elevator.direction == "up") {
-                            bestElevatorInformations = checkIfElevatorIsBetter(3, elevator, bestScore, referenceGap, bestElevator, requestedFloor);
+                            (bestElevator, bestScore, referenceGap) = checkIfElevatorIsBetter(3, elevator, bestScore, referenceGap, bestElevator, requestedFloor);
                             //The elevator is above me and is coming down. It means that I'm requesting an elevator to go to a floor, and the elevator is on it's way to me
                     }   else if (1 < elevator.currentFloor && elevator.direction == "down") {
-                            bestElevatorInformations = checkIfElevatorIsBetter(3, elevator, bestScore, referenceGap, bestElevator, requestedFloor); 
+                            (bestElevator, bestScore, referenceGap) = checkIfElevatorIsBetter(3, elevator, bestScore, referenceGap, bestElevator, requestedFloor); 
                             //The elevator is not at the first floor, but doesn't have any request
                     }   else if (elevator.status == "idle") {
-                            bestElevatorInformations = checkIfElevatorIsBetter(4, elevator, bestScore, referenceGap, bestElevator, requestedFloor);
+                            (bestElevator, bestScore, referenceGap) = checkIfElevatorIsBetter(4, elevator, bestScore, referenceGap, bestElevator, requestedFloor);
                             //The elevator is not available, but still could take the call if nothing better is found
                     }   else {
-                            bestElevatorInformations = checkIfElevatorIsBetter(5, elevator, bestScore, referenceGap, bestElevator, requestedFloor);
+                            (bestElevator, bestScore, referenceGap) = checkIfElevatorIsBetter(5, elevator, bestScore, referenceGap, bestElevator, requestedFloor);
                     }
                     // Elevator BestElevator = bestElevatorInformations.bestElevator;
                     // int bestScore = bestElevatorInformations.bestScore;
@@ -102,19 +102,19 @@ namespace Commercial_Controller
                         foreach (Elevator elevator in elevatorsList) {
                             //The elevator is at the same level as me, and is about to depart to the first floor
                             if (requestedFloor == elevator.currentFloor && elevator.status == "stopped" && requestedDirection == elevator.direction) {
-                                bestElevatorInformations = checkIfElevatorIsBetter(1, elevator, bestScore, referenceGap, bestElevator, requestedFloor);
+                                (bestElevator, bestScore, referenceGap) = checkIfElevatorIsBetter(1, elevator, bestScore, referenceGap, bestElevator, requestedFloor);
                                 //The elevator is lower than me and is going up. I'm on a basement, and the elevator can pick me up on it's way
                         }   else if (requestedFloor > elevator.currentFloor && elevator.direction == "up" && requestedDirection == "up") {
-                                bestElevatorInformations = checkIfElevatorIsBetter(2, elevator, bestScore, referenceGap, bestElevator, requestedFloor);   
+                                (bestElevator, bestScore, referenceGap) = checkIfElevatorIsBetter(2, elevator, bestScore, referenceGap, bestElevator, requestedFloor);   
                                 //The elevator is higher than me and is going down. I'm on a floor, and the elevator can pick me up on it's way 
                         }   else if (requestedFloor < elevator.currentFloor && elevator.direction == "down" && requestedDirection == "down") {
-                                bestElevatorInformations = checkIfElevatorIsBetter(2, elevator, bestScore, referenceGap, bestElevator, requestedFloor);
+                                (bestElevator, bestScore, referenceGap) = checkIfElevatorIsBetter(2, elevator, bestScore, referenceGap, bestElevator, requestedFloor);
                                 //The elevator is idle and has no requests    
                         }   else if (elevator.status == "idle") {
-                                bestElevatorInformations = checkIfElevatorIsBetter(4, elevator, bestScore, referenceGap, bestElevator, requestedFloor); 
+                                (bestElevator, bestScore, referenceGap) = checkIfElevatorIsBetter(4, elevator, bestScore, referenceGap, bestElevator, requestedFloor); 
                                 //The elevator is not available, but still could take the call if nothing better is found         
                         }   else {
-                            bestElevatorInformations = checkIfElevatorIsBetter(5, elevator, bestScore, referenceGap, bestElevator, requestedFloor);
+                            (bestElevator, bestScore, referenceGap) = checkIfElevatorIsBetter(5, elevator, bestScore, referenceGap, bestElevator, requestedFloor);
                         }
                         // Elevator BestElevator = bestElevatorInformations.bestElevator;
                         // int bestScore = bestElevatorInformations.bestScore;
@@ -122,12 +122,8 @@ namespace Commercial_Controller
                         
                     }       
                 }    
-    
-                
-            Elevator x = null;
-
-            return x;
-            // return bestElevator;
+            return bestElevator;
+            
         }
 
     
@@ -153,18 +149,29 @@ namespace Commercial_Controller
 
 
 
-        public  Elevator checkIfElevatorIsBetter(int scoreToCheck, Elevator newElevator, int bestScore, int referenceGap, Elevator bestElevator, int Floor ) {
+        public  (Elevator, int, int) checkIfElevatorIsBetter(int scoreToCheck, Elevator newElevator, int bestScore, int referenceGap, Elevator bestElevator, int floor ) {
+            if (scoreToCheck < bestScore) {
+                bestScore = scoreToCheck;
+                bestElevator = newElevator;
+                referenceGap = Math.Abs(newElevator.currentFloor - floor);
+            } else if (bestScore == scoreToCheck) {
+                int gap = Math.Abs(newElevator.currentFloor - floor);
+                if (referenceGap > gap){
+                    bestElevator = newElevator;
+                    referenceGap = gap;
+                }
             
-            Elevator bestElevatorInformations = null;
-            return bestElevatorInformations;
-
+            }
+            
+            return (bestElevator, bestScore, referenceGap);   
         }
         
 
 
 
 
-
+ 
+   
 
 
 
